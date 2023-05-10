67856228b4aaba63bcde2a4dc7dc73b70e2ca37f
Hamse-LibaaxMose <124046714+Hamse-LibaaxMose@users.noreply.github.com>
2023-05-10 14:14:57 +0200

    function showMember

--- js/script.js
+++ js/script.js
@@ -10,8 +10,27 @@ function start() {
 }
 
 function showMembers(array) {
+  document.querySelector(".member-container").innerHTML="";
+  
+  for (const member of array) {
+    showMember(member);
+  }
   
 
 }
 
-function showMember() {}
+function showMember(member) {
+  const html = /* HTML */ `
+  <article class="member-item">
+  <h3>${member.name}</h3>
+  <p>Alder: ${member.bday}</p>
+  <p>Tlf. Nummer: ${member.phonenumber}</p>
+  <p>Adress: ${member.adress} </p>
+  <p>Køn: ${member.gender}</p>
+  <p>Email: ${member.email}</p>
+  <p>Aktiv: ${member.active}</p>
+  <p>Konkurrence/motionist: ${member.competetive}</p>
+  <p>Trænere-id: ${member.trid} </p>
+  `
+
+}
