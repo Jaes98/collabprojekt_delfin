7e2a703f89ef3afe72b5b22349ea1cb0705df2aa
LisePetculescu <124042840+LisePetculescu@users.noreply.github.com>
2023-05-10 11:45:05 +0200

    rest functions

---
+++ js/REST.js
@@ -0,0 +1,19 @@
+"use strict";
+
+function getData() {}
+
+function prepareData() {}
+
+function createMember() {}
+
+function updateMember() {}
+
+function deleteMember() {}
+
+function showMembers() {}
+
+function showMember() {}
+
+function restSuccess() {}
+
+function restFail() {}
