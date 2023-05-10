f3bd5f1e71d8450d19756fdb2f29ca751aa2b3aa
LisePetculescu <124042840+LisePetculescu@users.noreply.github.com>
2023-05-10 13:09:25 +0200

    createNewMember + getData made

--- js/REST.js
+++ js/REST.js
@@ -1,10 +1,51 @@
 "use strict";
 
-function getData() {}
+const dolphinDatabase = "https://delfin-projekt-default-rtdb.europe-west1.firebasedatabase.app";
+
+async function getData() {
+  // henter data og omdanner til objekter
+  const membersFromDatabase = await fetch("https://delfin-projekt-default-rtdb.europe-west1.firebasedatabase.app/members.json");
+  const fetchedMembers = await membersFromDatabase.json();
+
+  // sætter members objekter ind i et array
+  const membersToArray = prepareData(fetchedMembers);
+
+  return membersToArray;
+}
 
 function prepareData() {}
 
-function createNewMember() {}
+async function createNewMember(member) {
+  const newMember = {
+    name,
+    bday,
+    phonenumber,
+    adress,
+    gender,
+    email,
+    active,
+    competetive,
+    crawl,
+    butterfly,
+    backCrawl,
+    breaststroke,
+    trid,
+  };
+
+  const json = JSON.stringify(newMember);
+
+  const response = await fetch(`${dolphinDatabase}/members.json`, { method: "POST", body: json });
+
+  if (response.status === 200) {
+    console.log("member was send to the database");
+    restSuccess();
+  } else {
+    console.error("member was NOT send to the database");
+    restFail();
+  }
+
+  return response;
+}
 
 function updateMember() {}
 
