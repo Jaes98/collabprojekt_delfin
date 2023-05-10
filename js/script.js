968013b3035a9d054f7ffa64ec2189229e047e7b
buan0001 <bustermikkel@gmail.com>
2023-05-10 17:47:51 +0200

    CSS til forms :))

--- js/script.js
+++ js/script.js
@@ -7,8 +7,21 @@ window.addEventListener("load", start);
 function start() {
   console.log("start:");
   viewControl();
+
+
+  document.querySelector("#formand-update-button").addEventListener("click",()=>document.querySelector("#formand-update-dialog").showModal())
+  document.querySelector("#formand-update-form").addEventListener("submit",updateMember)
 }
 
 function showMembers() {}
 
 function showMember() {}
+
+
+function updateMember(event) {
+  event.preventDefault()
+  console.log(event)
+
+  const value = event.target.value
+  console.log(value)
+}
