9dd0f1834580e4bee9a899c970f2beac8ccfc109
Jaes98 <marcus.jaes@gmail.com>
2023-05-11 09:52:12 +0200

    event listeners til delete/delete knapper

--- js/script.js
+++ js/script.js
@@ -11,7 +11,9 @@ function start() {
 
   document.querySelector("#formand-update-button").addEventListener("click", updateMemberClicked);
   document.querySelector("#formand-update-form").addEventListener("submit", updateMember);
-  document.querySelector("#form-delete-character").addEventListener("submit", deleteMemberYes);
+  document.querySelector("#form-delete-member").addEventListener("submit", deleteMemberYes);
+  document.querySelector("#btn-no-delete").addEventListener("click", () => document.querySelector("#dialog-delete-member").close()
+    );
 }
 
 function showMembers() {}
