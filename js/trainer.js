6c84e1e050875e730caced4bb50959b84ab02ff7
Jaes98 <marcus.jaes@gmail.com>
2023-05-19 18:06:39 +0200

    top5disciplines tables startet

--- js/trainer.js
+++ js/trainer.js
@@ -2,6 +2,16 @@
 
 function startTrainer(params) {
     // Starter alt med trainer
+    topFiveCrawl();
+    topFiveBreaststroke();
+    topFiveButterfly();
+    topFiveBackcrawl();
+}
+
+const topFiveMembers = [];
+
+for (const disciplines of listOfMembers) {
+  
 }
 
 function showMembersTrainer(array) {
@@ -12,5 +22,45 @@ function showMembersTrainer(array) {
       showMemberFormand(member);
     }
   }
+
+function topFiveCrawl() {
+  console.log("topFiveDisciplines IGANG DU");
+
+  const crawl = document.querySelector("#topfive-crawl-table-body");
+
+  crawl.insertAdjacentHTML(
+    "beforeend",
+
+  )
+}
+function topFiveBackcrawl() {
+  console.log("topFiveBackcrawl IGANG DU");
+
+  const backcrawl = document.querySelector("#topfive-backcrawl-table-body");
+
+  backcrawl.insertAdjacentHTML(
+    "beforeend",
+
+    );
+}
+function topFiveButterfly() {
+  console.log("topFiveButterfly IGANG DU");
+
+  const butterfly = document.querySelector("#topfive-butterfly-table-body");
+
+  butterfly.insertAdjacentHTML("beforeend");
+}
+function topFiveBreaststroke() {
+  console.log("topFiveBreaststroke IGANG DU");
+
+  const breaststroke = document.querySelector(
+    "#topfive-breaststroke-table-body"
+  );
+
+  breaststroke.insertAdjacentHTML(
+    "beforeend", 
+
+    );
+}
 
   export {startTrainer}
