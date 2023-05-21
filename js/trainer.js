e8f8fbe56ab4295e6ee92eff3f3e04fd482638bc
Jaes98 <marcus.jaes@gmail.com>
2023-05-21 13:46:05 +0200

    update fra lises

--- js/trainer.js
+++ js/trainer.js
@@ -16,10 +16,83 @@ async function updateResults() {
   showResultTrainer(resultater);
 }
 
-const topFiveMembers = [];
 
-for (const disciplines of listOfMembers) {
-  
+function showTopFiveMembers(array) {
+  const competitiveMembers = {
+    crawl: [],
+    backCrawl: [],
+    breaststroke: [],
+    butterfly: [],
+  };
+
+  for (const disciplines of listOfMembers) {
+  }
+  // Retrieve the members data from the database
+  const membersRef = firebase.database().ref("members");
+  membersRef
+    .once("value")
+    .then((snapshot) => {
+      const membersData = snapshot.val();
+
+      // Create an object to store members for each discipline
+      const disciplineMembers = {
+        crawl: [],
+        backCrawl: [],
+        breaststroke: [],
+        butterfly: [],
+      };
+
+      // Iterate over the members directly
+      for (const memberID in membersData) {
+        const member = membersData[memberID];
+
+        // Check if the member participates in each discipline and add them to the corresponding array
+        if (member.crawl) {
+          disciplineMembers.crawl.push(member);
+        }
+
+        if (member.backCrawl) {
+          disciplineMembers.backCrawl.push(member);
+        }
+
+        if (member.breaststroke) {
+          disciplineMembers.breaststroke.push(member);
+        }
+
+        if (member.butterfly) {
+          disciplineMembers.butterfly.push(member);
+        }
+      }
+
+      // Sort members in each discipline by their time
+      for (const discipline in disciplineMembers) {
+        disciplineMembers[discipline].sort((a, b) => {
+          // Assuming the time property exists in your member object
+          return a.time - b.time;
+        });
+      }
+
+      // Iterate over the sorted members in each discipline and display them in the HTML table
+      for (const discipline in disciplineMembers) {
+        const members = disciplineMembers[discipline];
+
+        // Display top 5 members for each discipline
+        for (let i = 0; i < Math.min(members.length, 5); i++) {
+          const member = members[i];
+
+          // Display member's information and result in the HTML table
+          const row = document.createElement("tr");
+          row.innerHTML = `<td>${discipline}</td><td>${member.name}</td><td>${member.email}</td><td>${member.time}</td>`;
+          table.appendChild(row);
+        }
+      }
+
+      // Append the table to the document body or any other desired location
+      document.body.appendChild(table);
+    })
+    .catch((error) => {
+      console.error("Failed to retrieve members data:", error);
+    });
 }
 
 
@@ -78,46 +151,6 @@ async function showMemberTrainer(result) {
   } else {
     console.error("for showMemberTrainer: something is wrong");
   }
-
-function topFiveCrawl() {
-  console.log("topFiveDisciplines IGANG DU");
-
-  const crawl = document.querySelector("#topfive-crawl-table-body");
-
-  crawl.insertAdjacentHTML(
-    "beforeend",
-
-  )
-}
-function topFiveBackcrawl() {
-  console.log("topFiveBackcrawl IGANG DU");
-
-  const backcrawl = document.querySelector("#topfive-backcrawl-table-body");
-
-  backcrawl.insertAdjacentHTML(
-    "beforeend",
-
-    );
-}
-function topFiveButterfly() {
-  console.log("topFiveButterfly IGANG DU");
-
-  const butterfly = document.querySelector("#topfive-butterfly-table-body");
-
-  butterfly.insertAdjacentHTML("beforeend");
-}
-function topFiveBreaststroke() {
-  console.log("topFiveBreaststroke IGANG DU");
-
-  const breaststroke = document.querySelector(
-    "#topfive-breaststroke-table-body"
-  );
-
-  breaststroke.insertAdjacentHTML(
-    "beforeend", 
-
-    );
-}
 }
 
 export { startTrainer };
