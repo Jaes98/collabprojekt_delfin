f2c685fc58b9a207aa6e96ba7ba21fdde6e9e87d
LisePetculescu <124042840+LisePetculescu@users.noreply.github.com>
2023-05-21 23:46:59 +0200

    trrainer css fixed

--- js/trainer.js
+++ js/trainer.js
@@ -1,26 +1,29 @@
 import { getUpdatedFirebase } from "./script.js";
 import { getResults } from "./REST.js";
 
-let resultater;
+let listOfResults;
 let listOfMembers;
 
 function startTrainer(array) {
   listOfMembers = array;
 
   updateResults();
+  memberOverviewTrainer();
 }
 
 async function updateResults() {
-  resultater = await getResults();
-  showResultTrainer(resultater);
+  listOfResults = await getResults();
+  showResultTrainer(listOfResults);
+  memberOverviewTrainer(listOfResults);
+  // console.log("###########", listOfResults);
 }
 
-function showResultTrainer(resultater) {
-  console.log("showResults array:", resultater);
+function showResultTrainer(results) {
+  console.log("showResults array:", results);
 
   document.querySelector("#trainer-table-body").innerHTML = "";
 
-  for (const result of resultater) {
+  for (const result of results) {
     showMemberTrainer(result);
     // console.log(result);
   }
@@ -33,8 +36,8 @@ async function showMemberTrainer(result) {
   dateToDato(result);
 
   // console.log("xxxx", result);
-
-  const html = /*html*/ `
+  if (member.active === "Aktivt medlem" && member.competetive === "Konkurrent") {
+    const html = /*html*/ `
       <tr class="member-item-kasserer">
       <td>${member.name}</td>
       <td>${member.ageGroup}</td>
@@ -46,7 +49,56 @@ async function showMemberTrainer(result) {
     </tr>
       `;
 
-  document.querySelector("#trainer-table-body").insertAdjacentHTML("beforeend", html);
+    document.querySelector("#trainer-table-body").insertAdjacentHTML("beforeend", html);
+  }
+}
+
+function memberOverviewTrainer() {
+  // checks active competition members
+  const countCompetitive = listOfMembers.filter((member) => member.competetive === "Konkurrent" && member.active === "Aktivt medlem");
+
+  // checks crawl members
+  const countCrawl = listOfResults.filter((result) => result.discipline === "crawl" && countCompetitive.some((member) => member.id === result.uid));
+  const countCrawlJunior = countCrawl.filter((result) => listOfMembers.some((member) => member.ageGroup === "Junior" && member.id === result.uid)).length;
+  const countCrawlSenior = countCrawl.filter((result) => listOfMembers.some((member) => (member.ageGroup === "Senior" || member.ageGroup === "Senior+") && member.id === result.uid)).length;
+
+  // checks backCrawl members
+  const countBackCrawl = listOfResults.filter((result) => result.discipline === "backCrawl" && countCompetitive.some((member) => member.id === result.uid));
+  const countBackCrawlJunior = countBackCrawl.filter((result) => listOfMembers.some((member) => member.ageGroup === "Junior" && member.id === result.uid)).length;
+  const countBackCrawlSenior = countBackCrawl.filter((result) => listOfMembers.some((member) => (member.ageGroup === "Senior" || member.ageGroup === "Senior+") && member.id === result.uid)).length;
+
+  // checks breaststroke members
+  const countBreaststroke = listOfResults.filter((result) => result.discipline === "breaststroke" && countCompetitive.some((member) => member.id === result.uid));
+  const countBreaststrokeJunior = countBreaststroke.filter((result) => listOfMembers.some((member) => member.ageGroup === "Junior" && member.id === result.uid)).length;
+  const countBreaststrokeSenior = countBreaststroke.filter((result) => listOfMembers.some((member) => (member.ageGroup === "Senior" || member.ageGroup === "Senior+") && member.id === result.uid)).length;
+
+  // checks butterfly members
+  const countButterfly = listOfResults.filter((result) => result.discipline === "butterfly" && countCompetitive.some((member) => member.id === result.uid));
+  const countButterflyJunior = countButterfly.filter((result) => listOfMembers.some((member) => member.ageGroup === "Junior" && member.id === result.uid)).length;
+  const countButterflySenior = countButterfly.filter((result) => listOfMembers.some((member) => (member.ageGroup === "Senior" || member.ageGroup === "Senior+") && member.id === result.uid)).length;
+
+  const overview = document.querySelector("#overview-trainer");
+  overview.innerHTML = "";
+  overview.insertAdjacentHTML(
+    "beforeend",
+    /*HTML */ `
+    <p><B>Antal medlemmer:</B> ${listOfMembers.length}</p>
+    <p><b>Aktive konkurrenter:</b> ${countCompetitive.length}</p>
+    <p><b>Crawl medlemmer:</b> ${countCrawl.length}</p>
+    <p>Junior: ${countCrawlJunior}</p>
+    <p>Senior: ${countCrawlSenior}</p>
+    <p><b>Rygcrawl medlemmer:</b> ${countBackCrawl.length}</p>
+    <p>Junior: ${countBackCrawlJunior}</p>
+    <p>Senior: ${countBackCrawlSenior}</p>
+    <p><b>Brystsvømning medlemmer:</b> ${countBreaststroke.length}</p>
+    <p>Junior: ${countBreaststrokeJunior}</p>
+    <p>Senior: ${countBreaststrokeSenior}</p>
+    <p><b>Butterfly medlemmer:</b> ${countButterfly.length}</p>
+    <p>Junior: ${countButterflyJunior}</p>
+    <p>Senior: ${countButterflySenior}</p>
+    `
+  );
+  // console.log(member.competetive);
 }
 
 let competition = "";
@@ -66,10 +118,9 @@ function disciplinesEngToDa(result) {
 
 let dato = "";
 function dateToDato(result) {
-const dates = result.date.split("-");
+  const dates = result.date.split("-");
 
-dato = dates[2] + "-" + dates[1] + "-" + dates[0];
-
+  dato = dates[2] + "-" + dates[1] + "-" + dates[0];
 }
 
 export { startTrainer };
