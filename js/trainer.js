e709b3ded79564ebc401e13f8d962b32701f9ea7
Jaes98 <marcus.jaes@gmail.com>
2023-05-23 13:48:09 +0200

    title fixed

--- js/trainer.js
+++ js/trainer.js
@@ -86,13 +86,14 @@ function topFiveMembers() {
 }
 function showTopFiveTables(topFive) {
   document.querySelector("#trainer-table-body").innerHTML = "";
-  // let lowerCaseString = valueToTopFiveBy.toLowerCase();
-  // let hyphenIndex = lowerCaseString.indexOf("-");
-  // let indexAfterHyphen = lowerCaseString.substring(hyphenIndex + 1);
-  // let titleCaseString = indexAfterHyphen.charAt(0).toUpperCase() + indexAfterHyphen.slice(1);
-  // let hyphenToSpaceString = valueToTopFiveBy.replace("-", " ");
-  // let finalString = hyphenToSpaceString.replace(indexAfterHyphen, titleCaseString);
-  document.querySelector("#trainer-h2").textContent = `Top 5` 
+  let lowerCaseString = valueToTopFiveBy.toLowerCase();
+  let hyphenIndex = lowerCaseString.indexOf("-");
+  let indexAfterHyphen = lowerCaseString.substring(hyphenIndex + 1);
+  let titleCaseString =indexAfterHyphen.charAt(0).toUpperCase() + indexAfterHyphen.slice(1);
+  let hyphenToSpaceString = valueToTopFiveBy.replace("-", " ");
+  let ageThing = hyphenToSpaceString.substring(0, 6);
+  let finalString = `${ageThing} ${titleCaseString}`;
+  document.querySelector("#trainer-h2").textContent = `Top 5 ${finalString}`; 
 
   const slicedTopFive = topFive.slice(0, 5);
   for (let index = 0; index < slicedTopFive.length; index++) {
